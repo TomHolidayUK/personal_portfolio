2f6c1966027c83f6255f192d11979b21d3947b27
TomHolidayUK <tom.holiday@mac.com>
2023-10-24 12:41:03 +0100

    24/10/23 2

--- src/Components/ScrollComponents/AutoScroll/AutoScroll.js
+++ src/Components/ScrollComponents/AutoScroll/AutoScroll.js
@@ -42,47 +42,32 @@ useEffect(() => {
 
   return (
     <div className='carousel2-container'>
-    <div className="carousel2">
-    {/* <img className="card card--active" src='./Logos/mbda.png' alt="MBDA Logo"/>
-    <img className="card card--left" src='./Logos/qinetiq.png' alt="Qinetic Logo"/>
-    <img className="card card--right" src='./Logos/blastech.png' alt="Blastech Logo"/> */}
-    
-    {cards.map((item, i) => {
-    const indexLeft = mod(index - 1, cards.length);
-    const indexRight = mod(index + 1, cards.length);
+      <div className="carousel2">
+      
+      {cards.map((item, i) => {
+      const indexLeft = mod(index - 1, cards.length);
+      const indexRight = mod(index + 1, cards.length);
 
-    let className = "card";
+      let className = "card";
 
-    if (i === index) {
-        className = "card card--active";
-    } else if (i === indexRight) {
-        className = "card card--right";
-    } else if (i === indexLeft) {
-        className = "card card--left";
-    } else className = "card";
+      if (i === index) {
+          className = "card card--active";
+      } else if (i === indexRight) {
+          className = "card card--right";
+      } else if (i === indexLeft) {
+          className = "card card--left";
+      } else className = "card";
 
-    return (
-        <img
-        key={item.id}
-        className={className}
-        src={item.image}
-        alt="Comic"
-        ></img>
-    );
-    })}
+      return (
+          <img
+          key={item.id}
+          className={className}
+          src={item.image}
+          alt="Comic"
+          ></img>
+      );
+      })}
     
-    
-    
-    {/* <img src='./Logos/ansys.png' alt="ansys Logo"/>
-    <img src='./Logos/BAE.png' alt="BAE Logo"/>
-    <img src='./Logos/dynalloy.png' alt="dynalloy Logo"/>
-    <img src='./Logos/ellis brigham.png' alt="ellis brigham Logo"/>
-    <img src='./Logos/expedition foods.webp' alt="expedition foods Logo"/>
-    <img src='./Logos/mission.png' alt="mission Logo"/>
-    <img src='./Logos/powertraveller.png' alt="powertraveller Logo"/>
-    <img src='./Logos/samaya.avif' alt="samaya Logo"/>
-    <img src='./Logos/sheffield.png' alt="sheffield Logo"/>
-    <img src='./Logos/torq.png' alt="torq Logo"/> */}
     </div>
 </div>
   );
